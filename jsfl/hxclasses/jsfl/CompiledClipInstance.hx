19d9a1580f3b45a8144ac62315c570a1d558d5df
Benjamin Dubois <bubble.etc@gmail.com>
2014-11-18 21:47:20 +0100

    Added en translation and javadoc

--- jsfl/hxclasses/jsfl/CompiledClipInstance.hx
+++ jsfl/hxclasses/jsfl/CompiledClipInstance.hx
@@ -1,78 +1,162 @@
 package jsfl;
 
+/**
+ *  The CompiledClipInstance object is essentially an instance of a movie clip that has been converted to a compiled clip library item
+ */
 @:native("CompiledClipInstance")
 extern class CompiledClipInstance extends Instance {
 	
-	// アクセシビリティパネルの「名前」フィールドと等価のストリング。
+	/**
+	 * A string that is equivalent to the Name field in the Accessibility panel.
+	 */
 	public var accName(default, default):String;
 	
-	// 「不透明」が選択されている場合にマットカラーを指定するストリング。
+	/**
+	 * A string that specifies the matte color when Opaque is selected.
+	 * Availability: Flash Professional CC.
+	 */
 	public var backgroundColor(default, default):String;
 	
-	// 描画モードを指定するストリング。
+	/**
+	 * A string that specifies the blend mode.
+	 * Availability: Flash Professional CC.
+	 */
 	public var blendMode(default, default):String;
 	
-	// 明るさのカラー効果プロパティインスペクターで設定されている値が含まれる整数。
+	/**
+	 * An int that contains the value set in the Color Effect Property Inspector for brightness.
+	 * Specify a percentage between -100 and 100.
+	 * Availability: Flash Professional CC.
+	 */
 	public var brightness(default, null):Int;
 	
-	// ビットマップをキャッシュするかどうかを示すブール値。
+	/**
+	 * A boolean that indicates whether to cache bitmaps.
+	 * The default is false 
+	 * Availability: Flash Professional CC.
+	 */
 	public var cacheAsBitmap(default, default):Bool;
 	
-	// 濃淡値とアルファ値を一定量ずつ増減させる整数。
+	/**
+	 * An int that reduces or increases the tint and alpha values by a constant amount
+	 * Valid values are -255 to 255.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorAlphaAmount(default, default):Int;
 	
-	// 濃淡値とアルファ値を指定した割合で減少させる整数。
+	/**
+	 * An int that reduces the tint and alpha values by a specified percentage.
+	 * Valid values are -100 to 100.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorAlphaPercent(default, default):Int;
 	
-	// 青の濃淡値を一定量ずつ増減させる整数。
+	/**
+	 * An int that reduces or increases the blue tint value by a constant amount.
+	 * Valid values are -255 to 255.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorBlueAmount(default, default):Int;
 	
-	// 青の濃淡値を指定した割合で減少させる整数。
+	/**
+	 * An int that reduces the blue tint value by a specified percentage.
+	 * Valid values are -100 to 100.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorBluePercent(default, default):Int;
 	
-	// 緑の濃淡値を一定量ずつ増減させる整数。
+	/**
+	 * An int that reduces or increases the green tint value by a constant amount.
+	 * Valid values are -255 to 255.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorGreenAmount(default, default):Int;
 	
-	// 緑の濃淡値を指定した割合で減少させる整数。
+	/**
+	 * An int that reduces the green tint value by a specified percentage.
+	 * Valid values are -100 to 100.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorGreenPercent(default, default):Int;
 	
-	// シンボルプロパティダイアログの設定に従ってカラーモードを指定するストリング。
+	/**
+	 * A string that specifies the color mode, as identified in the Symbol Properties dialog.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorMode(default, default):ColorMode;
 	
-	// 赤の濃淡値を一定量ずつ増減させる整数。
+	/**
+	 * An int that reduces or increases the red tint value by a constant amount.
+	 * Valid values are -255 to 255.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorRedAmount(default, default):Int;
 	
-	// 緑の濃淡値を指定した割合で減少させる整数。
+	/**
+	 * An int that reduces the green tint value by a specified percentage.
+	 * Valid values are -100 to 100.
+	 * Availability: Flash Professional CC.
+	 */
 	public var colorRedPercent(default, default):Int;
 	
-	// アクセシビリティパネルの「説明」フィールドと等価のストリング。
+	/**
+	 * A string that is equivalent to the Description field in the Accessibility panel.
+	 */
 	public var description(default, default):String;
 	
-	// Filter オブジェクトの配列。
+	/**
+	 * an array of Filter objects.
+	 * Availability: Flash Professional CC.
+	 */
 	public var filters(default, default):Array<Filter>;
 	
-	// 子オブジェクトのアクセシビリティを有効または無効にするブール値。
+	/**
+	 * a Boolean value that enables and disables the children of the object to be accessible.
+	 */
 	public var forceSimple(default, default):Bool;
 	
-	// アクセシビリティパネルの「ショートカット」フィールドと等しいストリング。
+	/**
+	 * a string that is equivalent to the Shortcut field in the Accessibility panel.
+	 * Not available for dynamic text fields.
+	 */
 	public var shortcut(default, default):String;
 	
-	// オブジェクトのアクセシビリティを有効または無効にするブール値。これは、アクセシビリティパネルの「オブジェクトをアクセス可能にする」の設定の逆ロジックと等価です。
+	/**
+	 * A Boolean value that enables or disables the accessibility of the object;
+	 * equivalent to the inverse logic of the Make Object Accessible setting in the Accessibility panel.
+	 */
 	public var silent(default, default):String;
 	
-	// アクセシビリティパネルの「タブインデックス」フィールドと等価の整数。
+	/**
+	 * An integer that is equivalent to the Tab Index field in the Accessibility panel.
+	 */
 	public var tabIndex(default, default):Int;
 	
-	// カラー効果プロパティインスペクターでスタイルの濃淡を使用している場合、濃淡に適用されるカラーを返す Color オブジェクト。
+	/**
+	 * A Color object that, when the Color Effect Property Inspector is using style tint, returns the color applied to the tint.
+	 * returns the tint percentage from -100 to 100
+	 * Availability: Flash Professional CC.
+	 */
 	public var tintColor(default, null):Int;
 	
-	// カラー効果プロパティインスペクターでスタイルの濃淡を使用している場合、濃淡の割合を返すストリング。
+	/**
+	 * A string that, when the Color Effect Property Inspector is using style tint,
+	 * returns the tint percentage.
+	 * Availability: Flash Professional CC.
+	 */
 	public var tintPercent(default, null):String;
 	
-	// 背景色を設定するブール値。
+	/**
+	 * A boolean that sets the background color:
+	 * true - Use 32-bit with alpha. false - Use the background color.
+	 * Availability: Flash Professional CC.
+	 */
 	public var useBackgroundColor(default, default):Bool;
 	
-	// 可視性を設定するブール値。
+	/**
+	 * A boolean that sets visibility
+	 * Availability: Flash Professional CC.
+	 */
 	public var visible(default, default):Bool;
 	
 }
